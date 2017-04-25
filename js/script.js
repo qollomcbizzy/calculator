46e899cbea7dbfe2ae625229b66d30b8222dca79
collo <qollomcbizzy64@gmail.com>
2017-04-25 10:22:18 +0300

    add the multiplication logic

--- js/script.js
+++ js/script.js
@@ -25,3 +25,12 @@ return number1/number2;
 };
 var result=division(number1,number2);
 alert(result);
+//this is for the MULTIPLICATION
+var number1=parseInt(prompt("enter the value of number one..?"));
+var number2=parseInt(prompt("enter the value of number two..?"));
+var multiplication=function(number1,number2)
+{
+return number1*number2;
+};
+var result=multiplication(number1,number2);
+alert(result);
