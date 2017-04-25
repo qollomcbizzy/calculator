c5a02460d9e616c7b8408998b4080440a94e1ec1
collo <qollomcbizzy64@gmail.com>
2017-04-25 10:19:16 +0300

    add the division logic

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//this is for the addition
 var number1=parseInt(prompt("enter the first number:"));;
 var number2=parseInt(prompt("enter the second number:"));
 var add=function(number1,number2)
@@ -6,6 +7,7 @@ return number1+number2;
 };
 var result=add(number1,number2)
 alert(result);
+//this is for the subtraction
 var number1=parseInt(prompt("enter the value of number one..?"));
 var number2=parseInt(prompt("enter the value of number two..?"));
 var subtract=function(number1,number2)
@@ -13,4 +15,13 @@ var subtract=function(number1,number2)
 return number1-number2;
 };
 var result=subtract(number1,number2);
+alert(result);
+//this is for the division
+var number1=parseInt(prompt("enter the value of number one..?"));
+var number2=parseInt(prompt("enter the value of number two..?"));
+var division=function(number1,number2)
+{
+return number1/number2;
+};
+var result=division(number1,number2);
 alert(result);
