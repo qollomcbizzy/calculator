d2ace2e26ab3343b0b07f9f5847b6883ddd197af
collo <qollomcbizzy64@gmail.com>
2017-04-25 10:12:40 +0300

    add the subtraction logic

--- js/script.js
+++ js/script.js
@@ -4,4 +4,13 @@ var add=function(number1,number2)
 {
 return number1+number2;
 };
-alert(add(number1,number2));
+var result=add(number1,number2)
+alert(result);
+var number1=parseInt(prompt("enter the value of number one..?"));
+var number2=parseInt(prompt("enter the value of number two..?"));
+var subtract=function(number1,number2)
+{
+return number1-number2;
+};
+var result=subtract(number1,number2);
+alert(result);
