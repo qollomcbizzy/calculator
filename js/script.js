273431574bc96416d9e35b5b635eb1cbd37483de
collo <qollomcbizzy64@gmail.com>
2017-04-27 15:31:29 +0300

    added the user interfance to show the results in the html for the add

--- js/script.js
+++ js/script.js
@@ -3,30 +3,27 @@ var add=function(number1,number2)
 {
 return number1+number2;
 };
-var result=add(number1,number2)
-alert(result);
 //this is for the subtraction
 var subtract=function(number1,number2)
 {
 return number1-number2;
 };
-var result=subtract(number1,number2);
-alert(result);
 //this is for the division
 var division=function(number1,number2)
 {
 return number1/number2;
 };
-var result=division(number1,number2);
-alert(result);
 //this is for the MULTIPLICATION
 var multiplication=function(number1,number2)
 {
 return number1*number2;
 };
-var result=multiplication(number1,number2);
-alert(result);
 $(document).ready(function(){
-  var number1=parseInt($("#add1").val());;
+  $("form#add").submit(function(event){
+    event.preventDefault();
+  var number1=parseInt($("#add1").val());
   var number2=parseInt($("#add2").val());
+  var result=add(number1,number2);
+  $("#output").text(result);
+});
 });
