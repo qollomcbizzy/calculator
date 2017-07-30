d7020c69ffb6c8fd0ecf6b3442d179fa75ec71ed
collo <qollomcbizzy64@gmail.com>
2017-07-30 13:51:01 +0300

    added the buid enviroment and the production environment

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
+var utilities = require('gulp-util');
 var gulp =require('gulp');
 var concat =require('gulp-concat');
 var browserify = require('browserify');
 var uglify=require('gulp-uglify');
 var source = require('vinyl-source-stream');
+var buildProduction = utilities.env.production;
+//creates an environment for our build
 gulp.task('concatInterface',function(){
   return gulp.src(['./js/calculator-interface.js','./js/response.js'])
   .pipe(concat('allconcact.js'))
@@ -29,4 +32,13 @@ gulp.task('minifyscripts',['calcBrowserify'],function(){
   return gulp.src('./build/js/app.js')
   .pipe(uglify())
   .pipe(gulp.dest('./build/js'));
+});
+//lets create a build task that does after we have minified
+gulp.task('build',function() {
+  if(buildProduction){
+    gulp.start('minifyscripts');
+  }
+  else{
+    gulp.start('calcBrowserify');
+  }
 });
