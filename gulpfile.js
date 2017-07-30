3123ea874bdc8d66870a3f84c633def4dd86d126
collo <qollomcbizzy64@gmail.com>
2017-07-30 13:15:32 +0300

    concatenated and browserified both the response file and the calculator-interface

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 var gulp =require('gulp');
+var concat =require('gulp-concat');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+gulp.task('concatInterface',function(){
+  return gulp.src(['./js/calculator-interface.js','./js/response.js'])
+  .pipe(concat('allconcact.js'))
+  .pipe(gulp.dest('./tmp'))
+});
+//used when browserify the calculator interface only
+/*
 gulp.task('calcBrowserify', function() {
   return browserify({ entries: ['./js/calculator-interface.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
+});
+*/
+//lets now modify it with two file the response and calculator-interface
+gulp.task('calcBrowserify',['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allconcact.js'] })
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
--- tmp/allconcact.js
+++ tmp/allconcact.js
@@ -1,31 +1,3 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-//backend or the business logic
-//this is for the addition
-function calc(skinName){
-  this.skin=skinName
-}
-calc.prototype.add=function(number1,number2)
-{
-return number1+number2;
-};
-//this is for the subtraction
-calc.prototype.subtract=function(number1,number2)
-{
-return number1-number2;
-};
-//this is for the division
-calc.prototype.division=function(number1,number2)
-{
-return number1/number2;
-};
-//this is for the MULTIPLICATION
-calc.prototype.multiplication=function(number1,number2)
-{
-return number1*number2;
-};
-exports.calcModule=calc;
-
-},{}],2:[function(require,module,exports){
 var calc=require("./../js/script.js").calcModule;
 $(document).ready(function(){
   //for addition
@@ -82,5 +54,3 @@ $(document).ready(function(){
         "for your repsonse we really appreciate that for our improvement</p>");
   });
 });
-
-},{"./../js/script.js":1}]},{},[2]);
