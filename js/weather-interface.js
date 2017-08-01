edf9d995f148f455791f2f994b46528d18b78c36
collo <qollomcbizzy64@gmail.com>
2017-08-01 13:58:16 +0300

    added the the then() function of the promises

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -3,12 +3,12 @@ $(document).ready(function(){
   $("#getweather").click(function(){
     var inputtedCity=$("input#yourcity").val();
    $("#yourcity").val("");
-   $("#showweather").text("the city that you entered is"+inputtedCity+"<br />");
    //the get takes two arguement the link and the response that would be brought back
    $.get('http://api.openweathermap.org/data/2.5/weather?q='
-    + inputtedCity + '&appid=' + apiKey, function(response) {
-     console.log(response);
-     console.log(JSON.stringify(response));
+    + inputtedCity + '&appid=' + apiKey)
+    //we introduce the then () function called promises thats if the first is fullfiled we can continue
+    .then (function(response) {
+     $("#showweather").text("the city that you entered is"+inputtedCity + "Its humidity is" + response.main.humidity + "%");
    });
   })
 });
